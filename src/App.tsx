36d92d8d7869d5f978e9da381eae1a7da0dcfdc4
ifiokInyang <ifiok.inyang@decagon.dev>
2023-07-29 13:28:06 +0100

    implemented the login and sign up pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
 import React, { useEffect } from "react";
 import { Route, BrowserRouter, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home/Home";
 // import { Toaster } from "react-hot-toast";
 
 function AppRoutes() {
-  const { pathname } = useLocation();
+	const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
 
-  return (
-    <React.Fragment>
-      <Routes>
-        {/* <Toaster /> */}
-        {/* <Route path="/" element={< />} /> */}
-      </Routes>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Routes>
+				{/* <Toaster /> */}
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</React.Fragment>
+	);
 }
 
 function App() {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<AppRoutes />
+		</BrowserRouter>
+	);
 }
 export default App;
